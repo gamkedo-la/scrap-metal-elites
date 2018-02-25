71654eba2e30a8dae604423a55d695f5f6ae334c
Cameron Button <csbutton01@hotmail.com>
2018-02-25 07:36:03 -0700

    pre meeting changes, main menu added, enemy bot attributes tweaked

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void NewGameBtn(string startNewGame)
+    {
+        SceneManager.LoadScene(startNewGame);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
