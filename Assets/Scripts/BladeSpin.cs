dcf2a68e3733542ab8605b2030f122b0428caf63
Cameron Button <csbutton01@hotmail.com>
2018-03-01 15:41:54 -0700

    added a saw blade and created a BladeSpin script so that it may spin on mouse click

---
+++ Assets/Scripts/BladeSpin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeSpin : MonoBehaviour
+{
+    public float spinSpeed = 5.0f;
+    
+    
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Debug.Log("mouse down");
+            transform.Rotate(Vector3.forward * Time.deltaTime * -spinSpeed);
+        }
+	}
+}
