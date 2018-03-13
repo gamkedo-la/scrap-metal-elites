ab86c21f95cce47047acaf620cf66f9201c0299f
Tylor Allison <tylor@foobox.com>
2018-03-13 10:24:14 -0500

    added initial component health script as well as impact damage applicator

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Health : MonoBehaviour {
+
+    [System.Serializable]
+    public class OnHealthValueEvent : UnityEvent<int> { };
+    public class OnDeathEvent : UnityEvent<GameObject> { };
+
+    public int maxHealth = 100;
+    public bool debug;
+    public OnHealthValueEvent onChange;
+    public OnHealthValueEvent onChangePercent;
+    public OnDeathEvent onDeath;
+
+    private int health;
+
+    void Awake() {
+        onChange = new OnHealthValueEvent();
+        onChangePercent = new OnHealthValueEvent();
+        onDeath = new OnDeathEvent();
+        health = maxHealth;
+        // cannot have zero or negative max health
+        if (maxHealth < 1) {
+            maxHealth = 1;
+        }
+    }
+
+    public void TakeDamage(int amount, GameObject from) {
+        if (debug) {
+            Debug.Log("Taking damage: " + amount + " from: " + from);
+        }
+        // if health is already zero (or zero damage), can't take more damage
+        if (health == 0 || amount == 0) return;
+
+        // apply damage
+        health -= amount;
+
+        // check for death
+        if (health <= 0) {
+            health = 0;
+            onDeath.Invoke(from);
+            if (debug) {
+                Debug.Log("Death from: " + from);
+            }
+        }
+
+        // communicate health change
+        onChange.Invoke(health);
+        var percent = health/maxHealth;
+        onChangePercent.Invoke(percent);
+    }
+
+}
---
+++ Assets/Scripts/ImpactDamageApplicator.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections;
+
+public class ImpactDamageApplicator : MonoBehaviour {
+    public float damageModifier = 1f;
+    // damage thresholds
+    public float minDamage = 10f;
+    public float maxDamage = 1000f;
+    public bool debug = false;
+
+    //AudioSource audioSource;
+    private Health health;
+
+    void Start() {
+        //audioSource = GetComponent<AudioSource>();
+        health = GetComponent<Health>();
+        // enforce max damage > min damage
+        if (maxDamage < minDamage) {
+            maxDamage = minDamage;
+        }
+    }
+
+    void DebugCollision(Collision collision) {
+        // Debug-draw all contact points and normals
+        foreach (ContactPoint contact in collision.contacts) {
+            Debug.DrawRay(contact.point, contact.normal, Color.red, 3);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        // compute damage (impulse * velocity * modifier)
+        var damage = collision.impulse.magnitude * collision.relativeVelocity.magnitude * damageModifier;
+
+        if (debug) {
+            Debug.Log(gameObject.name + " OnCollisionEnter\n impulse: " + collision.impulse.magnitude +
+                                        " velocity: " + collision.relativeVelocity.magnitude +
+                                        " times: " + (collision.impulse.magnitude * collision.relativeVelocity.magnitude) +
+                                        " computed damage: " + damage);
+            DebugCollision(collision);
+        }
+
+        // apply damage thresholds
+        if (damage < minDamage) {
+            if (debug) {
+                Debug.Log("collision damage does not minimum");
+            }
+            return;
+        }
+        if (damage > maxDamage) {
+            damage = maxDamage;
+        }
+
+        // apply damage to component health
+        if (health != null) {
+            health.TakeDamage(Mathf.RoundToInt(damage), collision.gameObject);
+        }
+
+        /*
+        // Play a sound if the colliding objects had a big impact.
+        if (collision.relativeVelocity.magnitude > 2) {
+            audioSource.Play();
+        }
+        */
+    }
+}
