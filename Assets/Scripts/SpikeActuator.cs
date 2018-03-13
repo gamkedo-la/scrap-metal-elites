427298302d7e451c845b21b155ada87ed8db8665
Tylor Allison <tylor@foobox.com>
2018-03-12 20:52:18 -0500

    add spike module, simple actuator script

---
+++ Assets/Scripts/SpikeActuator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeActuator : MonoBehaviour {
+
+    void Update () {
+    	if (Input.GetKeyDown(KeyCode.Alpha1)) {
+    	    GetComponent<Animation>().Play("Armature|ArmatureAction");
+    	}
+    }
+}
