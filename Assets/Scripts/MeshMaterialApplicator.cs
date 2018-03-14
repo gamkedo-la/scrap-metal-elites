b4da535bda9f28436e08043e08621c064efea8fa
Tylor Allison <tylor@foobox.com>
2018-03-13 21:03:37 -0500

    add simple mesh distributor/applicator scripts to send mesh material updates through a bot

---
+++ Assets/Scripts/MeshMaterialApplicator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshMaterialApplicator : MonoBehaviour {
+    private Renderer[] renderers;
+
+    public Material material {
+        set {
+            if (renderers != null) {
+                for (var i=0; i<renderers.Length; i++) {
+                    if (renderers[i] != null) renderers[i].material = value;
+                }
+            }
+        }
+    }
+
+    void Start() {
+        renderers = GetComponents<Renderer>();
+    }
+}
---
+++ Assets/Scripts/MeshMaterialDistributor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshMaterialDistributor : MonoBehaviour {
+    public Material material;
+
+    private MeshMaterialApplicator[] applicators;
+    private bool firstUpdate = true;
+
+    void Start() {
+        applicators = GetComponentsInChildren<MeshMaterialApplicator>();
+        Debug.Log("# material applicators: " + applicators.Length);
+    }
+
+
+    void SetMaterials() {
+        if (applicators == null) return;
+        for (var i=0; i<applicators.Length; i++) {
+            applicators[i].material = material;
+        }
+    }
+
+    void Update() {
+        if (firstUpdate) {
+            SetMaterials();
+            firstUpdate = false;
+        }
+    }
+
+}
