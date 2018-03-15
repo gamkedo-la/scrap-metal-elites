2cc7428f5b6874b7af503f309b65e16e66a8202d
Tylor Allison <tylor@foobox.com>
2018-03-15 00:08:00 -0500

    wip: new bot w/ damage scripts and breakable joints

---
+++ Assets/Scripts/MassCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MassCalculator : MonoBehaviour {
+    void Start() {
+        float totalMass = 0;
+        var rbs = GetComponentsInChildren<Rigidbody>();
+        for (var i=0; i<rbs.Length; i++) {
+            //Debug.Log(rbs[i].gameObject.name + " mass " + rbs[i].mass);
+            totalMass += rbs[i].mass;
+        }
+
+        Debug.Log(gameObject.name + " # rbs: " + rbs.Length + " totalMass: " + totalMass);
+    }
+}
