8531ce50d28b383ca62e48dc5a349d3108fd48a8
deepbluezen <deepbluezen@users.noreply.github.com>
2018-03-23 22:12:31 +0000

    Flamethrower control added, initial collision based damage set up

---
+++ Assets/FireFlamethrower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlamethrower : MonoBehaviour {
+
+    public ParticleSystem Fire;
+    public ParticleSystem Smoke;
+    public ParticleSystem Plume;
+
+    	
+	void Update () {
+
+        if (Input.GetButton("Fire1"))
+        {
+            Fire.Emit(1);
+            Smoke.Emit(1);
+            Plume.Emit(2);
+        }
+
+	}
+}
---
+++ Assets/FlameDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameDamage : MonoBehaviour {
+
+    //public GameObject target;
+    private GameObject target;
+    private float FireRate = 0.4f;
+    public float FireDamage = 100f;
+    public ParticleSystem plume;
+
+    // Use this for initialization
+    void Start () {
+
+        target = GetComponent<GameObject>();
+        
+	}
+
+    private void OnParticleCollision(GameObject plume)
+    {
+
+        FireDamage -= FireRate;
+        Debug.Log("Health left:"+FireDamage);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
