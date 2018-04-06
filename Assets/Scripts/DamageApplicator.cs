345a522dca97ebfaec7c4f9e1f5af4893b8f6afe
Tylor Allison <tylor@foobox.com>
2018-04-06 08:47:11 -0700

    first take on integrating flame damage w/ component health scripts

--- Assets/Scripts/DamageApplicator.cs
+++ Assets/Scripts/DamageApplicator.cs
@@ -4,11 +4,32 @@ using System.Collections;
 [CreateAssetMenu(fileName = "damageVariable", menuName = "Variable/Damage")]
 public class DamageApplicator : ComponentApplicator {
     // damage sources
+    [Tooltip("Should this part react to impact damage")]
     public bool impactApplyDamage;
+    [Tooltip("Minimum impact damage threshold")]
     public float impactMinDamage;
+    [Tooltip("Max impact damage threshold")]
     public float impactMaxDamage;
+    [Tooltip("Scale factor for incoming damage, lower value means less damage")]
     public float impactDamageModifier;
+
+    [Tooltip("Should this part react to fire damage")]
     public bool fireApplyDamage;
+    [Tooltip("Damage to apply to health for each flame particle collision")]
+    private float fireRate = 1f;
+    [Tooltip("The flame plume particle that we will accept damage from on collision")]
+    public ParticleSystem plume;
+    [Tooltip("Spark particle to emit after exploded")]
+    public ParticleSystem sparks;
+    [Tooltip("New material to add once burnt")]
+    public Material burntMaterial;
+    [Tooltip("Percent of health to fall under before applying burn")]
+    public int burnThreshold = 50;
+    [Tooltip("Percent of health to fall under before applying explode")]
+    public int explodeThreshold = 10;
+    [Tooltip("Amount of force to apply during explosion")]
+    public float explodeForce = 1000;
+    public bool debug;
 
     public override void Apply(PartConfig config, GameObject target) {
         if (target == null) return;
@@ -17,14 +38,25 @@ public class DamageApplicator : ComponentApplicator {
         var rigidbodyGo = PartUtil.GetBodyGo(target);
         if (rigidbodyGo == null) return;
 
-        // apply damage actuat
+        // apply impact damage actuator
         if (impactApplyDamage) {
             var actuator = rigidbodyGo.AddComponent<ImpactDamageActuator>();
             actuator.minDamage = impactMinDamage;
             actuator.maxDamage = impactMaxDamage;
             actuator.damageModifier = impactDamageModifier;
+            actuator.debug = debug;
         }
 
-        // FIXME: fire damage
+        // apply fire damage applicator
+        if (fireApplyDamage) {
+            var actuator = rigidbodyGo.AddComponent<FlameDamageActuator>();
+            actuator.fireRate = fireRate;
+            actuator.plume = plume;
+            actuator.sparks = sparks;
+            actuator.burntMaterial = burntMaterial;
+            actuator.burnThreshold = burnThreshold;
+            actuator.explodeThreshold = explodeThreshold;
+            actuator.debug = debug;
+        }
     }
 }
---
+++ Assets/Scripts/FlameDamageActuator.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameDamageActuator : MonoBehaviour {
+
+    public float fireRate = 0.6f;
+    public ParticleSystem plume;
+    public ParticleSystem sparks;
+    public Material burntMaterial;
+    public int burnThreshold = 50;
+    public int explodeThreshold = 10;
+    public float explodeForce = 1000f;
+    public bool debug;
+
+    private Rigidbody rb;
+    private Health health;
+    private Material unburntMaterial;
+    private Transform emitlocation;
+    private bool is_burnt;
+    private bool is_exploded;
+
+    void Start () {
+        // link health to our on percent change handler
+        health = GetComponent<Health>();
+        if (health != null) {
+            health.onChangePercent.AddListener(OnHealthPercentChange);
+        }
+        unburntMaterial = GetComponent<Renderer>().material;
+        rb = GetComponent<Rigidbody>();
+        emitlocation = gameObject.transform;
+	}
+
+    private void OnParticleCollision(GameObject other)
+    {
+
+        //List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+        //int numCollisionEvents = plume.GetCollisionEvents(other, collisionEvents);
+        var damage = fireRate;
+
+        if (debug) {
+            Debug.Log(gameObject.name + " OnParticleCollision\n" +
+                                        " particle: " + other.name +
+                                        " fireRate: " + fireRate +
+                                        " damage: " + damage);
+        }
+
+        if (health != null) {
+            health.TakeDamage(Mathf.RoundToInt(damage), other);
+        }
+
+    }
+
+    void Burn() {
+        if (debug) {
+            Debug.Log(gameObject.name + " burning");
+        }
+        is_burnt = true;
+        if (unburntMaterial != null && burntMaterial != null) {
+            unburntMaterial.CopyPropertiesFromMaterial(burntMaterial);
+        }
+    }
+
+    void Explode() {
+        if (debug) {
+            Debug.Log(gameObject.name + " exploding");
+        }
+        is_exploded = true;
+        if (rb != null) {
+            rb.AddForce(transform.up * explodeForce);
+        }
+    }
+
+    void OnHealthPercentChange(int newValue) {
+        if (debug) {
+            Debug.Log("FlameDamage OnHealthPercentChange: " + newValue);
+        }
+        // burnt threshold
+        if (!is_burnt && newValue<burnThreshold) {
+            Burn();
+        }
+        if (!is_exploded && newValue<explodeThreshold) {
+            Explode();
+        }
+    }
+
+    void Update () {
+        if (is_exploded)
+        {
+            sparks.transform.position = emitlocation.position;
+            sparks.Emit(5);
+        }
+    }
+}
