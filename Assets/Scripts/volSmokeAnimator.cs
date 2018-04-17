a72ce74002760920e4de9e203c888a811b637bd6
MaesterLazer <daniel.l.lazar@gmail.com>
2018-04-16 21:38:45 -0700

    WIP Volumetric smoke cone (for lighting)

---
+++ Assets/Scripts/volSmokeAnimator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class volSmokeAnimator : MonoBehaviour {
+
+    public Texture[] textures;
+    public Material smokeMat;
+    public int frameFactor = 1;
+    int fFCount = 0;
+    public int texSwitcher = 0;
+
+   
+
+	void FixedUpdate () {
+        fFCount++;
+        if (fFCount > frameFactor) {
+            fFCount = 0;
+        }
+        if (fFCount == frameFactor) {
+            texSwitcher++;
+            if (texSwitcher > (textures.Length - 1)) {
+                texSwitcher = 0;
+            }
+            smokeMat.SetTexture("_MainTex", textures[texSwitcher]);
+        }
+
+	}
+}
